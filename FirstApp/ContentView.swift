48a05c8c13ad9f52442bb81a4a5e7179b57a11b2
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2022-12-15 21:31:25 -0300

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Carlos Kimura on 15/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ByeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
